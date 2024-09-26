523b485a1be76c788d5bc114c8e3497acad36dae
Muhamad Iksan <muhamadiksan351@gmail.com>
2024-09-26 09:40:36 +0700

    update video player & reusable api call

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,3 +1,4 @@
+import { getAnimeResponse } from '@/app/libs/api-libs';
 import AnimeList from '@/components/AnimeList';
 import Header from '@/components/AnimeList/Header';
 
@@ -6,19 +7,19 @@ const Page = async ({ params }) => {
 
   const decodedKeyword = decodeURI(keyword);
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`
-  );
-  const topAnime = await response.json();
+  // const response = await fetch(
+  //   `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${decodedKeyword}`
+  // );
+  // const topAnime = await response.json();
+
+  const searchAnime = await getAnimeResponse('anime', `q=${decodedKeyword}`);
 
   return (
     <>
       {/* Anime terpopuler */}
       <section>
-        <Header
-          title={`Pencarian Untuk ${decodedKeyword}...`}
-        />
-        <AnimeList api={topAnime} />
+        <Header title={`Pencarian Untuk ${decodedKeyword}...`} />
+        <AnimeList api={searchAnime} />
       </section>
     </>
   );
